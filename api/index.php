979d644e8b6d43f0c07ae4df1ca2ac2349288920
zhiyang <zhangntesos@163.com>
2016-04-16 11:04:02 +0800

    complete the basic functions

--- api/index.php
+++ api/index.php
@@ -12,8 +12,10 @@ $app->put('/users/:id', 'updateUser');
 $app->delete('/users/:id',	'deleteUser');
 
 $app->post('/login', 'login');
+$app->post('/register', 'register');
 
-
+$app->post('/bonddevice', 'bondDevice');
+$app->post('/unbonddevice', 'unbondDevice');
 
 $app->get('/','getAll');
  
@@ -21,6 +23,9 @@ $app->get('/gps/:userId', 'getGps');
 $app->get('/gps/:userId/:n', 'getLatestNGps'); 
 $app->get('/gps/:userId/:start/:end', 'getGpsBetween');
 $app->post('/gps', 'addGps');
+
+$app->get('/cellbase/:deviceId', 'getCellBase'); 
+$app->post('/cellbase', 'addCellBase');
 
 $app->post('/test', 'addTest');
 $app->get('/test/:a', function($a){
@@ -67,7 +72,6 @@ function addUser() {
 	error_log('\naddUser\n', 3, '/var/tmp/php.log');
 	$request = Slim::getInstance()->request();
 	$user = json_decode($request->getBody());
-	echo $request->getBody();
 	$sql = "INSERT INTO user (id, name, sex, debug) VALUES (:id, :name, :sex, :debug)";
 	try {
 		$db = getConnection();
@@ -135,16 +139,68 @@ function findByName($query) {
 	}
 }
 
+function checkUserExist($email)
+{
+	error_log('\ncheckuser\n', 3, '/var/tmp/php.log');
+	$sql = "SELECT * FROM user WHERE email=:email";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("email", $email);
+		$stmt->execute();
+		$user = $stmt->fetchObject();  
+		$db = null;
+		if(!empty($user)) return 1;
+		else return 0;
+	
+	} catch(PDOException $e) {
+		return 2;
+	}
+	return 0;
+}
+
+function register() {
+	error_log('\nregister\n', 3, '/var/tmp/php.log');
+	$request = Slim::getInstance()->request();
+	$user = json_decode($request->getBody());
+	if(checkUserExist($user->email) != 0) 
+	{
+        	echo '{"id":"0"}'; 
+		return;
+	}
+
+
+	$sql = "INSERT INTO user (id, name, email, password, phoneNumber, sex, debug) VALUES (:id, :name, :email, :password, :phoneNumber,  :sex, :debug)";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $user->id);
+		$stmt->bindParam("name", $user->name);
+		$stmt->bindParam("email", $user->email);
+		$stmt->bindParam("password", $user->password);
+		$stmt->bindParam("phoneNumber", $user->phoneNumber);
+		$stmt->bindParam("sex", $user->sex);
+		$stmt->bindParam("debug", $user->debug);
+		$stmt->execute();
+		$user->id = $db->lastInsertId();
+		$db = null;
+		echo json_encode($user); 
+	} catch(PDOException $e) {
+		error_log($e->getMessage(), 3, '/var/tmp/php.log');
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
 
 function login() {
 	error_log('\nlogin\n', 3, '/var/tmp/php.log');
 	$request = Slim::getInstance()->request();
 	$param = json_decode($request->getBody());
-	$sql = "SELECT * FROM user WHERE phoneNumber=:phoneNumber";
+	$sql = "SELECT * FROM user WHERE email=:email and password=:password";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);  
-		$stmt->bindParam("phoneNumber", $param->phoneNumber);
+		$stmt->bindParam("email", $param->email);
+		$stmt->bindParam("password", $param->password);
 		$stmt->execute();
 		$user = $stmt->fetchObject();  
 		$db = null;
@@ -154,32 +210,82 @@ function login() {
 	}
 }
 
+function bondDevice() {
+	error_log('\nbonddevice\n', 3, '/var/tmp/php.log');
+	$request = Slim::getInstance()->request();
+	$param = json_decode($request->getBody());
+	$sql = "UPDATE user SET deviceId=:deviceId, deviceSim=:deviceSim where id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $param->userId);
+		$stmt->bindParam("deviceId", $param->deviceId);
+		$stmt->bindParam("deviceSim", $param->deviceSim);
+		$stmt->execute();
+		$db = null;
+		echo '{"ok":1}'; 
+	} catch(PDOException $e) {
+		error_log($e->getMessage(), 3, '/var/tmp/php.log');
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function unbondDevice() {
+	error_log('\nunbonddevice\n', 3, '/var/tmp/php.log');
+	$request = Slim::getInstance()->request();
+	$param = json_decode($request->getBody());
+	$sql = "UPDATE user SET deviceId=:deviceId, deviceSim=:deviceSim where id=:id";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("id", $param->userId);
+		$stmt->bindParam("deviceId", 0);
+		$stmt->bindParam("deviceSim", 0);
+		$stmt->execute();
+		$db = null;
+		echo '{"ok":1}'; 
+	} catch(PDOException $e) {
+		error_log($e->getMessage(), 3, '/var/tmp/php.log');
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
 
 
 function getGps($userId) {
-	$sql = "SELECT * FROM gps WHERE userId=:userId";
+	$sql = "SELECT * FROM user WHERE id=:userId";
+	$sql1 = "SELECT * FROM gps WHERE deviceId=:deviceId";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);  
 		$stmt->bindParam("userId", $userId);
 		$stmt->execute();
-		//while($gps=$stmt->fetch());
-		$gpsAll = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$user = $stmt->fetchObject();  
+
+		$stmt1 = $db->prepare($sql1);
+		$stmt1->bindParam("deviceId",$user->deviceId);
+		$stmt1->execute();
+		$gpsAll = $stmt1->fetchAll(PDO::FETCH_OBJ);
 	        $gps = array_pop($gpsAll);		
 		$db = null;
 		echo json_encode($gps);
-		//echo '{"gps": ' . json_encode($gps) . '}';
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
 	}
 }
 
+
 function getGpsBetween($userId, $start, $end) {
-	$sql = "SELECT * FROM gps WHERE userId=:userId and timeStamp between :start and :end";
+	$sql = "SELECT * FROM gps WHERE deviceId=:deviceId and timeStamp between :start and :end";
+	$sql1 = "SELECT * FROM user WHERE id=:userId";
 	try {
 		$db = getConnection();
+		$stmt1 = $db->prepare($sql);  
+		$stmt1->bindParam("userId", $userId);
+		$stmt1->execute();
+		$user = $stmt->fetchObject();  
+
 		$stmt = $db->prepare($sql);  
-		$stmt->bindParam("userId", $userId);
+		$stmt->bindParam("deviceId", $user->deviceId);
 		$stmt->bindParam("start", $start);
 		$stmt->bindParam("end", $end);
 		$stmt->execute();
@@ -194,12 +300,17 @@ function getGpsBetween($userId, $start, $end) {
 function getLatestNGps($userId, $n)
 {
 
-	$sql = "SELECT * FROM gps WHERE userId=:userId";
-	echo $sql;
+	$sql = "SELECT * FROM gps WHERE deviceId=:deviceId";
+	$sql1 = "SELECT * FROM user WHERE id=:userId";
 	try {
 		$db = getConnection();
+		$stmt1 = $db->prepare($sql1);  
+		$stmt1->bindParam("userId", $userId);
+		$stmt1->execute();
+		$user = $stmt1->fetchObject();  
+
 		$stmt = $db->prepare($sql);  
-		$stmt->bindParam("userId", $userId);
+		$stmt->bindParam("deviceId", $user->deviceId);
 		$stmt->execute();
 		$allGps = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$gps = array_slice($allGps, count($allGps) - $n, count($allGps) - 1);
@@ -209,16 +320,20 @@ function getLatestNGps($userId, $n)
 		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
 	}
 }
+
+
+
 function addGps() {
 	error_log('\naddGps\n', 3, '/var/tmp/php.log');
 	$request = Slim::getInstance()->request();
 	$gps = json_decode($request->getBody());
-	$sql = "INSERT INTO gps (userId, id, timeStamp, latitude, longtitude, altitude, hAccuracy, vAccuracy, speed) VALUES (:userId, :id, :timeStamp, :latitude, :longtitude, :altitude, :hAccuracy, :vAccuracy, :speed)";
+	$sql = "INSERT INTO gps (userId, id,deviceId, timeStamp, latitude, longtitude, altitude, hAccuracy, vAccuracy, speed) VALUES (:userId, :id, :deviceId, :timeStamp, :latitude, :longtitude, :altitude, :hAccuracy, :vAccuracy, :speed)";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);  
 		$stmt->bindParam("userId", $gps->userId);
 		$stmt->bindParam("id", $gps->id);
+		$stmt->bindParam("deviceId", $gps->deviceId);
 		$stmt->bindParam("timeStamp", $gps->timeStamp);
 		$stmt->bindParam("latitude", $gps->latitude);
 		$stmt->bindParam("longtitude", $gps->longtitude);
@@ -236,6 +351,52 @@ function addGps() {
 	}
 }
 
+
+function getCellBase($deviceId) {
+	$sql = "SELECT * FROM cellbase WHERE deviceId=:deviceId";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("deviceId", $deviceId);
+		$stmt->execute();
+		//while($gps=$stmt->fetch());
+		$cellBaseAll = $stmt->fetchAll(PDO::FETCH_OBJ);
+	        $cellBase = array_pop($cellBaseAll);		
+		$db = null;
+		echo json_encode($cellBase);
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function addCellBase() {
+	error_log('\naddCellBase\n', 3, '/var/tmp/php.log');
+	
+	$request = Slim::getInstance()->request();
+	$cellbase = json_decode($request->getBody());
+	$sql = "INSERT INTO cellbase (userId, id, deviceId, timeStamp, mcc, mnc, lac, cid) VALUES (:userId, :id,:deviceId, :timeStamp, :mcc, :mnc, :lac, :cid)";
+
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);  
+		$stmt->bindParam("userId", $cellbase->userId);
+		$stmt->bindParam("id", $cellbase->id);
+		$stmt->bindParam("deviceId", $cellbase->deviceId);
+		$stmt->bindParam("timeStamp", $cellbase->timeStamp);
+		$stmt->bindParam("mcc", $cellbase->mcc);
+		$stmt->bindParam("mnc", $cellbase->mnc);
+		$stmt->bindParam("lac", $cellbase->lac);
+		$stmt->bindParam("cid", $cellbase->cid);
+		$stmt->execute();
+		$cellbase->id = $db->lastInsertId();
+		$db = null;
+		echo json_encode($cellbase); 
+	} catch(PDOException $e) {
+		error_log($e->getMessage(), 3, '/var/tmp/php.log');
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+
+}
 function addTest() {
 	error_log('\naddTest\n', 3, '/var/tmp/php.log');
 	$request = Slim::getInstance()->request();
